6571f9bd9c316b26d2f744f2363121d478116133
WloMac <wlodzimierz.maciaszczyk@gmail.com>
2026-05-07 13:05:49 +0100

    final build v0.0.1 - spelling changes

--- src/data/hero.js
+++ src/data/hero.js
@@ -1,5 +1,5 @@
 export const heroData = {
     title: "Oddychaj. Czuj. Bądź.",
-    subtitle: "Odkryj przestrzeń, w której możesz być sobą — w ciele, w emocjach, w pełnej obecności",
+    subtitle: "Odkryj przestrzeń, w której możesz być sobą — w ciele, w emocjach, w pełnej obecności.",
     buttonText: "Zapisz się na zajęcia",
 }
